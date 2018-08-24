302eda92a39c57a8802b496bb442bc408d3dc4cb
franck <franck_gxu@outlook.com>
2018-08-24 11:43:58 +0800

    hahah

---
+++ spider_framework/jianshu/start.py
@@ -0,0 +1,9 @@
+"""
+# -*- coding: utf-8 -*-
+# @Time    : 2018/8/21 17:18
+# @Author  : franck
+# @Email   : franck_gxu@outlook.com
+"""
+
+from scrapy import cmdline
+cmdline.execute("start crawl jsspider".split())
